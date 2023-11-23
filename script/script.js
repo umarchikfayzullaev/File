dd61cdd5d5fdf95ea477340fc8bf6aa107a9c6c9
Umarchik <umarjonfayzullaev2004@gmail.com>
2023-11-23 23:47:48 +0500

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function toggleDiv() {
+    var div = document.getElementById("myDiv");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
